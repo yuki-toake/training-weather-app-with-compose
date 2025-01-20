3e0997fd8b59fd35640544e905e506baef19bd7a
yuki-toake <yuki-toake@greensys.co.jp>
2025-01-20 23:10:44 +0900

    :cake:chore(deps): bump coil from 2.7.0 to 3.0.4

--- core/designsystem/src/main/kotlin/jp/co/greensys/weather/app/core/designsystem/component/DynamicAsyncImage.kt
+++ core/designsystem/src/main/kotlin/jp/co/greensys/weather/app/core/designsystem/component/DynamicAsyncImage.kt
@@ -1,17 +1,22 @@
 package jp.co.greensys.weather.app.core.designsystem.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalInspectionMode
-import coil.compose.AsyncImagePainter.State.Error
-import coil.compose.rememberAsyncImagePainter
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImagePainter.State.Error
+import coil3.compose.rememberAsyncImagePainter
+import jp.co.greensys.weather.app.core.designsystem.theme.WeatherTheme
 
 @Composable
 fun DynamicAsyncImage(
@@ -27,12 +32,28 @@ fun DynamicAsyncImage(
             isError.value = state is Error
         },
     )
-    val isLocalInspection = LocalInspectionMode.current
 
-    Image(
-        contentScale = ContentScale.Crop,
-        painter = if (isError.value.not() && !isLocalInspection) imageLoader else placeholder,
-        contentDescription = contentDescription,
+    Box(
         modifier = modifier,
-    )
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            contentScale = ContentScale.Crop,
+            painter = if (isError.value.not()) imageLoader else placeholder,
+            contentDescription = contentDescription,
+            modifier = Modifier.matchParentSize(),
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun DynamicAsyncImagePreview() {
+    WeatherTheme {
+        DynamicAsyncImage(
+            imageUrl = "",
+            contentDescription = null,
+            modifier = Modifier.size(size = 150.dp),
+        )
+    }
 }
